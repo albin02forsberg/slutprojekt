31f4f139f44fcc8a262143b0724e2664f400ccfa
Albin Forsberg <albin02forsberg@gmail.com>
2021-03-27 22:12:02 +0100

    Playing around with get and post requests

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import {
   Route,
   Link,
   useRouteMatch,
-  useParams
+  useParams,
 } from "react-router-dom";
 
 export default function App() {
@@ -14,32 +14,58 @@ export default function App() {
     <Router>
       <nav class="navbar navbar-expand-xl navbar-light bg-light">
         <div class="container-fluid">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo1" aria-controls="navbarTogglerDemo1" aria-expanded="false" aria-label="Toggle navigation">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo1"
+            aria-controls="navbarTogglerDemo1"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse show" id="navbarTogglerDemo1">
-            <a class="navbar-brand" href="/">Coach Network</a>
+            <a class="navbar-brand" href="/">
+              Coach Network
+            </a>
             <ul class="navbar-nav me-auto mb-2 mb-xl-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/">Feed</a>
+                <a class="nav-link active" aria-current="page" href="/">
+                  Feed
+                </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/createex">Exercise creator</a>
+                <a class="nav-link active" aria-current="page" href="/createex">
+                  Exercise creator
+                </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/trainingplanner">Training planner</a>
+                <a
+                  class="nav-link active"
+                  aria-current="page"
+                  href="/trainingplanner"
+                >
+                  Training planner
+                </a>
               </li>
             </ul>
             <form class="d-flex">
               <ul class="navbar-nav me-auto mb-2 mb-xl-0">
                 <li class="nav-item">
-                  <a class="nav-link" href="/login">Login</a>
+                  <a class="nav-link" href="/login">
+                    Login
+                  </a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="/signup">signup</a>
+                  <a class="nav-link" href="/signup">
+                    signup
+                  </a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="/account">My account</a>
+                  <a class="nav-link" href="/account">
+                    My account
+                  </a>
                 </li>
               </ul>
             </form>
@@ -64,21 +90,21 @@ export default function App() {
 }
 
 function Home() {
-
   const [test, testSet] = useState("");
 
   useEffect(() => {
-    axios.get('http://localhost:3001/testAPI')
+    axios
+      .get("http://localhost:3001/testAPI")
       .then(function (response) {
         // handle success
         testSet(response.data);
-        console.log(response)
+        console.log(response);
       })
       .catch(function (error) {
         // handle error
         console.log(error);
-      })
-  }, [])
+      });
+  }, []);
 
   return (
     <div class="container">
@@ -99,6 +125,33 @@ function Login() {
   );
 }
 
+function test() {
+  let name = document.getElementById("name").value;
+  let username = document.getElementById("username").value;
+  let mail = document.getElementById("mail").value;
+  let password1 = document.getElementById("password1").value;
+  let password2 = document.getElementById("password2").value;
+
+  let user = {
+    name: name,
+    username: username,
+    mail: mail,
+    password1: password1,
+    password2: password2
+  }
+
+  // Send get request to server for validation
+
+  axios.get("http://localhost:3001/testAPI")
+    .then(res => {
+      console.log(res.data)
+    });
+
+  // Post data to server for db and auth if user details meets criteria
+  axios.post("http://localhost:3001/testAPI", user);
+  console.log(user);
+}
+
 function Signup() {
   return (
     <div class="container">
@@ -106,20 +159,55 @@ function Signup() {
         <h1>Signup</h1>
       </div>
       <div class="col-md-6">
-        <form>
-          <lable class="form-lable" >Name</lable>
-          <input type="text" class="form-control" id="name" name="name" required placeholder="Name"></input>
-          <lable class="form-lable" >Username</lable>
-          <input type="text" class="form-control" id="username" name="username" required placeholder="Username"></input>
-          <lable class="form-lable">Mail</lable>
-          <input type="email" class="form-control" id="mail" name="mail" required placeholder="Mail"></input>
-          <lable class="form-lable">Password</lable>
-          <input type="password" class="form-control" id="password1" name="password1" required placeholder="password"></input>
-          <lable class="form-lable">Confirm password</lable>
-          <input type="password" class="form-control" id="password2" name="password2" required placeholder="Confirm password"></input>
-          <br></br>
-          <button class="btn btn-success">Submit</button>
-        </form>
+        <lable class="form-lable">Name</lable>
+        <input
+          type="text"
+          class="form-control"
+          id="name"
+          name="name"
+          required
+          placeholder="Name"
+        ></input>
+        <lable class="form-lable">Username</lable>
+        <input
+          type="text"
+          class="form-control"
+          id="username"
+          name="username"
+          required
+          placeholder="Username"
+        ></input>
+        <lable class="form-lable">Mail</lable>
+        <input
+          type="email"
+          class="form-control"
+          id="mail"
+          name="mail"
+          required
+          placeholder="Mail"
+        ></input>
+        <lable class="form-lable">Password</lable>
+        <input
+          type="password"
+          class="form-control"
+          id="password1"
+          name="password1"
+          required
+          placeholder="password"
+        ></input>
+        <lable class="form-lable">Confirm password</lable>
+        <input
+          type="password"
+          class="form-control"
+          id="password2"
+          name="password2"
+          required
+          placeholder="Confirm password"
+        ></input>
+        <br></br>
+        <button class="btn btn-success" onClick={test}>
+          Submit
+        </button>
       </div>
     </div>
   );
--- server/routes/testAPI.js
+++ server/routes/testAPI.js
@@ -1,11 +1,17 @@
 let express = require("express");
 let router = express.Router();
 
-router.get("/", function(req, res, next){
-    res.send("API is working!");
+router.get("/", function(req, res, next) {
+    let user = {
+        name: "Albin",
+        username: "test",
+        mail: "testmail"
+    }
+
+    res.send(user);
 });
 
-router.post("/", function(req,res){
+router.post("/", function(req, res) {
     console.log(req.body);
     res.send(req.body);
 });
