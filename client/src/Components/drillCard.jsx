0cda1bef9e017e38f5a9a7f26ff66dacb67f26fe
Albin Forsberg <albin02forsberg@gmail.com>
2021-05-11 22:10:56 +0200

    Small changes

--- client/src/Components/drillCard.jsx
+++ client/src/Components/drillCard.jsx
@@ -1,20 +1,25 @@
-import react from "react";
+import react, { useEffect, useState } from "react";
 
 function DrillCard(prop) {
   let link = "/drill/" + prop.id;
+
+  let [date] = useState("");
+
   return (
-      <div className="card" onClick={()=> window.location.replace(link)}>
-        <div className="card-body">
-          <h4 className="card-title">
-            <a href={link}>{prop.name}</a>
-          </h4>
-          <div className="card-text">
-            <p>{prop.type}: {prop.moment}</p> 
-            <p>{prop.level}</p> 
-          </div>
+    <div className="card" onClick={() => window.location.replace(link)}>
+      <div className="card-body">
+        <h4 className="card-title">
+          <a href={link}>{prop.name}</a>
+        </h4>
+        <div className="card-text">
+          <p>
+            {prop.type}: {prop.moment}
+          </p>
+          <p>{prop.level}</p>
         </div>
-        <div className="card-footer">{prop.created} </div>
       </div>
+      <div className="card-footer">{prop.created.substring(0,10)} - <a href={"http://localhost:3000/user/" + prop.creator}>{prop.creator}</a></div>
+    </div>
   );
 }
 
